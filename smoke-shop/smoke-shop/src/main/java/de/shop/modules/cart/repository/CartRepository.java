7a7867d219d568ca2fd1f70015c4157b3b1c4ce3
Valerija Toncinska <valerija.toncinska12@gmail.com>
2024-07-25 10:51:50 +0300

    Add Product service, add Cart

---
+++ smoke-shop/smoke-shop/src/main/java/de/shop/modules/cart/repository/CartRepository.java
@@ -0,0 +1,4 @@
+package de.shop.modules.cart.repository;
+
+public class CartRepository {
+}
--- smoke-shop/smoke-shop/src/main/java/de/shop/modules/product/service/ProductServiceImpl.java
+++ smoke-shop/smoke-shop/src/main/java/de/shop/modules/product/service/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package de.shop.modules.product.service;
 
+import de.shop.core.components.LanguageResolver;
 import de.shop.core.components.ResponseDto;
 import de.shop.modules.product.domain.dto.ProductDto;
 import de.shop.modules.product.domain.entity.ProductEntity;
@@ -8,12 +9,14 @@ import de.shop.modules.product.service.mapping.ProductMappingService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Properties;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository repository;
     private final ProductMappingService mappingService;
+    private LanguageResolver lang;
 
     public ProductServiceImpl(ProductRepository repository, ProductMappingService mappingService) {
         this.repository = repository;
@@ -42,13 +45,13 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ResponseDto<ProductDto> findById(Long id) {
+        Properties p = lang.load("product", "messages.properties");
         ProductEntity product = repository.findById(id).orElse(null);
         if (product == null || !product.isActive()) {
-            return new ResponseDto<>(false, null,
-                    String.format("Product with id - %d does not exist", id), "eng");
+            return new ResponseDto<>(false, null, p.get("product_not_found_by_id").toString(), "eng");
         } else {
             ProductDto dto = mappingService.mapEntityToDto(product);
-            return new ResponseDto<>(true, dto, "Product found successfully", "eng");
+            return new ResponseDto<>(true, dto, p.get("product_found").toString(), "eng");
         }
     }
 
