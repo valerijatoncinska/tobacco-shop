1ff4ba6b2362e0119ee8d0d56901daedd0fe99ab
Valerija Toncinska <valerija.toncinska12@gmail.com>
2024-08-06 13:06:10 +0300

    let all users view products (catalog)

--- smoke-shop/src/main/java/de/shop/modules/product/controller/ProductControllerAll.java
+++ smoke-shop/src/main/java/de/shop/modules/product/controller/ProductControllerAll.java
@@ -5,33 +5,30 @@ import de.shop.modules.product.service.ProductService;
 import de.shop.modules.product.service.ProductServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/products")
-//@Secured({"ROLE_USER", "ROLE_ADMIN"})
-public class ProductController {
+public class ProductControllerAll {
 
     private final ProductService service;
     private ProductServiceImpl serviceImpl;
 
-    public ProductController(ProductService service, ProductServiceImpl serviceImpl) {
+    public ProductControllerAll(ProductService service, ProductServiceImpl serviceImpl) {
         this.service = service;
         this.serviceImpl = serviceImpl;
     }
 
-    // Добавляем в корзину продукт.
-    @PostMapping("/{id}/addition-to-cart")
-    public ResponseEntity<?> addToCart(@PathVariable Long id) {
-        boolean n = serviceImpl.addItemCart(id);
-        if (n == true) {
-            return ResponseEntity.ok("ok");
-        } else {
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        }
+    @GetMapping("/{id}")
+    public OutputProductAdminDto findByIdForUser(@PathVariable Long id) {
+        return service.findByIdForUser(id);
+    }
+
+    @GetMapping
+    public List<OutputProductAdminDto> findAllActiveProductsForUsers() {
+        return service.findAllActiveProductsForUsers();
     }
 
 }
