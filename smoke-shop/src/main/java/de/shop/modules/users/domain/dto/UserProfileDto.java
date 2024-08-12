75910a00d784c99c4124f582e0d7e54ddcc1cc20
Valerija Toncinska <valerija.toncinska12@gmail.com>
2024-08-12 15:16:10 +0300

    fix Optional property in findByEmail for user in AuthController

--- smoke-shop/src/main/java/de/shop/modules/users/domain/dto/UserProfileDto.java
+++ smoke-shop/src/main/java/de/shop/modules/users/domain/dto/UserProfileDto.java
@@ -1,14 +1,11 @@
 package de.shop.modules.users.domain.dto;
 import de.shop.modules.users.domain.entity.RoleEntity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 
-public class UserProfileDto implements UserDetails {
+public class UserProfileDto {
 
     private Long id;
 
@@ -20,38 +17,7 @@ public class UserProfileDto implements UserDetails {
 
     private Set<AuthorityDto> authorities = new HashSet<>();
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-
-    @Override
-    public String getUsername() {
-        return "";
-    }
-
     private Long cartId;
-
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 
     public void setId(Long id) {
         this.id = id;
@@ -59,10 +25,6 @@ public class UserProfileDto implements UserDetails {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public void setRoles(Set<RoleEntity> roles) {
@@ -83,11 +45,6 @@ public class UserProfileDto implements UserDetails {
 
     public String getEmail() {
         return email;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
     }
 
     public Set<RoleEntity> getRoles() {
